8a845a1c5d1fc7d2f4b6f92eda24a241d024d851
Dule Martins <dulemartins07@gmail.com>
2020-06-07 08:53:01 +0100

    Please enter the commit message for your changes. Lines starting

---
+++ scripts/duleM.js
@@ -0,0 +1,8 @@
+//HNGi7 task2
+
+const email = "dulemartins07@gmail.com";
+const name = "Tabah Baridule";
+const hng_id = "HNG-02033";
+const language = "javascript";
+
+console.log(`Hello World, this is ${name} with HNGi7 ID ${hng_id} using ${language} for stage 2 task email ${email}`)
